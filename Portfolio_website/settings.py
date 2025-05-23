f93ba2025a5b7a31e91261ba639165f4e40716cf
salonig90 <giratkarsaloni1@gmail.com>
2025-05-23 15:04:48 +0530

    final commit

--- Portfolio_website/settings.py
+++ Portfolio_website/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-habk+$7m7_%!co-34w#h+t(z^+u_zbf@-9zglm23ekmc!9v370
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -130,5 +130,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
  
-#Activate Django-heroku
-django_heroku.settings(locals())
